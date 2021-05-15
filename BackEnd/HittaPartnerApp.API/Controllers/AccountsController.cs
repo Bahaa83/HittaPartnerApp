1de0a2d9a9a28f9175febee01bb82f7e13a9c07c
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-16 00:24:11 +0200

    Skapa Accounts kontroller

---
+++ BackEnd/HittaPartnerApp.API/Controllers/AccountsController.cs
@@ -0,0 +1,22 @@
+﻿using HittaPartnerApp.API.Services.IRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HittaPartnerApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAuthentication _repo;
+
+        public AccountsController(IAuthentication repo)
+        {
+            _repo = repo;
+        }
+    }
+}

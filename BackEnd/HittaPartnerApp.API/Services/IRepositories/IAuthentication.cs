7daec1f030dfecd6ed066b8c766b8e837223053f
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-15 21:28:13 +0200

    Bygga Interface för Inloggnings authentication

---
+++ BackEnd/HittaPartnerApp.API/Services/IRepositories/IAuthentication.cs
@@ -0,0 +1,15 @@
+﻿using HittaPartnerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HittaPartnerApp.API.Services.IRepositories
+{
+    public interface IAuthentication
+    {
+        Task<User> Register(User user, string Password);
+        Task<User> Login(string UserName, string Password);
+        Task<bool> IsExists(string UserName);
+    }
+}

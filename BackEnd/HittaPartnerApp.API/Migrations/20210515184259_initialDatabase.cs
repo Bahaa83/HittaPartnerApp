2abe6a20bf6c47040cde2d03254323d093eb15fb
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-15 20:43:58 +0200

    Lägg till Migration och update-database

---
+++ BackEnd/HittaPartnerApp.API/Migrations/20210515184259_initialDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HittaPartnerApp.API.Migrations
+{
+    public partial class initialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}

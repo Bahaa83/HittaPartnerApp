1f96fae550f856f840d6854d9f5354142418d189
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-15 19:49:55 +0200

    Registerar HittaPartnerDbContex i ConfiguretionServices

--- BackEnd/HittaPartnerApp.API/Startup.cs
+++ BackEnd/HittaPartnerApp.API/Startup.cs
@@ -1,7 +1,9 @@
+using HittaPartnerApp.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,10 @@ namespace HittaPartnerApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<HittaPartnerDbContext>(options=>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddCors();
             services.AddSwaggerGen(Options=>
             {

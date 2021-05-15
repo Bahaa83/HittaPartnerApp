a9b97bfdb4c168b9e9f65976e15db11df24cb2a4
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-15 21:31:17 +0200

    Bygga Repository för inloggnings Authentication

---
+++ BackEnd/HittaPartnerApp.API/Services/Repositories/Authentication.cs
@@ -0,0 +1,28 @@
+﻿using HittaPartnerApp.API.Services.IRepositories;
+using HittaPartnerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HittaPartnerApp.API.Services.Repositories
+{
+    public class Authentication : IAuthentication
+    {
+        
+        public async Task<bool> IsExists(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Login(string UserName, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string Password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

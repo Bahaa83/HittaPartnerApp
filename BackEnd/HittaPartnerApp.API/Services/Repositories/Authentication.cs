a045f2b11ef86ece461d118723fa6ccd229ebeaf
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-16 00:09:05 +0200

    Utförande Inloggnings funktion i Authentication repository

--- BackEnd/HittaPartnerApp.API/Services/Repositories/Authentication.cs
+++ BackEnd/HittaPartnerApp.API/Services/Repositories/Authentication.cs
@@ -1,6 +1,7 @@
 ﻿using HittaPartnerApp.API.Data;
 using HittaPartnerApp.API.Services.IRepositories;
 using HittaPartnerApp.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +19,35 @@ namespace HittaPartnerApp.API.Services.Repositories
         }
         public async Task<bool> IsExists(string UserName)
         {
-            throw new NotImplementedException();
+            
+            if (await db.users.AnyAsync(x => x.UserName.Equals(UserName))) return true;
+            return false;
         }
 
         public async Task<User> Login(string UserName, string Password)
         {
-            throw new NotImplementedException();
+            var user = await db.users.FirstOrDefaultAsync(x => x.UserName.Equals(UserName));
+            if (user == null) return null;
+            if ( !VerifyPasswordHash(Password, user.PasswordHash, user.PasswordSalt)) return null;
+            return user;
+
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+               
+               var ComputedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < ComputedHash.Length; i++)
+                {
+                    if(ComputedHash[i]!= passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
 
         public async Task<User> Register(User user, string Password)

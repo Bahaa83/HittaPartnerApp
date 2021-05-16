fbc453762694cabee2ca380dea2adf30dd5151b0
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-16 20:24:09 +0200

    Lägg till Authentication i

--- BackEnd/HittaPartnerApp.API/Startup.cs
+++ BackEnd/HittaPartnerApp.API/Startup.cs
@@ -1,6 +1,7 @@
 using HittaPartnerApp.API.Data;
 using HittaPartnerApp.API.Services.IRepositories;
 using HittaPartnerApp.API.Services.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,11 +11,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HittaPartnerApp.API
@@ -38,6 +42,17 @@ namespace HittaPartnerApp.API
             });
             services.AddCors();
             services.AddScoped<IAuthentication, Authentication>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(Options =>
+                {
+                    Options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
             services.AddSwaggerGen(Options=>
             {
                 Options.SwaggerDoc("HittaPartnerOpenAPISpec", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -54,8 +69,36 @@ namespace HittaPartnerApp.API
                 var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.Xml";
                 var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
                 Options.IncludeXmlComments(cmlCommentsFullPath);
+            //    // Lägg till JWT token inom Swagger .
+            //    Options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            //    {
+            //        Description=
+            //        "Jwt Authorization Header useing the Bearer Schena.\r\r\r\n Enter 'Bearer '[mellanslag]'och sedan din token i texttimatning nedan, ",
+            //        Name="Authorization",
+            //        In=Microsoft.OpenApi.Models.ParameterLocation.Header,
+            //        Type=Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+            //        Scheme="Bearer"
+            //    });
+            //    Options.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+            //        {new OpenApiSecurityScheme
+            //        {
+            //            Reference= new OpenApiReference
+            //            {
+            //                Type= ReferenceType.SecurityScheme,
+            //                Id="Bearer"
+            //            },
+            //            Scheme="oauth2",
+            //            Name="Bearer",
+            //            In=ParameterLocation.Header,
+                       
+            //        },
+            //        new List<string>()
+            //        }
                 
-            });
+            //    });
+              
+                
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,7 +119,7 @@ namespace HittaPartnerApp.API
 
             app.UseRouting();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

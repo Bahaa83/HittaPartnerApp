823b46d51ca6b60060763db7ea33463b969836ba
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-16 02:57:21 +0200

    Lägg till validations inom UserForRegisterDto model

--- BackEnd/HittaPartnerApp.API/Services/DtoModels/UserForRegisterDto.cs
+++ BackEnd/HittaPartnerApp.API/Services/DtoModels/UserForRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace HittaPartnerApp.API.Services.DtoModels
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string UserName { get; set; }
+        [StringLength(10,MinimumLength =4,ErrorMessage ="Låsenördet får inte bestå av mer än tiotecken och inte mindre än fyra")]
         public string Password { get; set; }
     }
 }

a3dbfacf12994270cf6c248491c717717002e49b
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-16 02:31:09 +0200

    Bygga registreringstjänst inom AccountsController

--- BackEnd/HittaPartnerApp.API/Controllers/AccountsController.cs
+++ BackEnd/HittaPartnerApp.API/Controllers/AccountsController.cs
@@ -1,4 +1,6 @@
-﻿using HittaPartnerApp.API.Services.IRepositories;
+﻿using HittaPartnerApp.API.Services.DtoModels;
+using HittaPartnerApp.API.Services.IRepositories;
+using HittaPartnerApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,6 +12,11 @@ namespace HittaPartnerApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+
+
     public class AccountsController : ControllerBase
     {
         private readonly IAuthentication _repo;
@@ -17,6 +24,22 @@ namespace HittaPartnerApp.API.Controllers
         public AccountsController(IAuthentication repo)
         {
             _repo = repo;
+        }
+        [HttpPost("Register")]
+        [ProducesResponseType(201)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult>Register(UserForRegisterDto userForRegisterDto)
+        {
+
+            if (await _repo.IsExists(userForRegisterDto.UserName.ToLower()))
+                return BadRequest("Den här användaren är finns redan");
+            var newUser = new User()
+            {
+                UserName= userForRegisterDto.UserName
+            };
+            var CreatedUser = await _repo.Register(newUser, userForRegisterDto.Password);
+            if (CreatedUser == null) return BadRequest();
+            return StatusCode(201);
         }
     }
 }

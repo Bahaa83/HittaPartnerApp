ec06668a817c8aac1cdfde281a1bf950b6f5f2cf
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-20 16:01:09 +0200

    Lägg till Http module och FormsModule

--- FrontEnd/HittaPartnerApp-Client/src/app/navbar/navbar.component.ts
+++ FrontEnd/HittaPartnerApp-Client/src/app/navbar/navbar.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+model:any={}
   constructor() { }
 
   ngOnInit() {
   }
-
+login(){
+  console.log(this.model)
+}
 }

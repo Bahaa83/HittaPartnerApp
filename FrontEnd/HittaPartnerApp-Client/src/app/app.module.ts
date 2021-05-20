48a8f3d74726d81cb198ba7f0e1bc9d7582de421
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-20 16:09:56 +0200

    Binding i login form

--- FrontEnd/HittaPartnerApp-Client/src/app/app.module.ts
+++ FrontEnd/HittaPartnerApp-Client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +15,9 @@ import { NavbarComponent } from './navbar/navbar.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

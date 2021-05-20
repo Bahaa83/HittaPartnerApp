24f72174f6dfe58fdaf66e3641fee433b755ddc2
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-20 19:23:38 +0200

    Lägg till Account Services i clientapp

---
+++ FrontEnd/HittaPartnerApp-Client/src/app/_services/account.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, ɵINJECTOR_IMPL__POST_R3__ } from '@angular/core';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+baseUrl='https://localhost:44350/api/Accounts/'
+constructor(private http:HttpClient) { }
+
+login(model:any){
+return this.http.post(this.baseUrl+'Login',model).pipe(
+  map((response:any)=>{
+    const user=response;
+    if(user){
+      localStorage.setItem('token',user.token);
+    }
+  })
+)
+}
+}

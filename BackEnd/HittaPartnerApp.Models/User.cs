47bf83cd6661383422f97f771de9747ef2dcf73d
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-30 15:50:01 +0200

    Lägga till mer kunskapar

--- BackEnd/HittaPartnerApp.Models/User.cs
+++ BackEnd/HittaPartnerApp.Models/User.cs
@@ -9,5 +9,15 @@ namespace HittaPartnerApp.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction  { get; set; }
+        public string LookingFor { get; set; }
+        public string Inerests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }

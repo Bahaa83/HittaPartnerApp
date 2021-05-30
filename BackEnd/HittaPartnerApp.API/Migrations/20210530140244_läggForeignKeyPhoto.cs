4d24ab313a13ae8757d8f84268397ec051bc6f5d
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-05-30 16:03:19 +0200

    Add migration add foreign key

---
+++ BackEnd/HittaPartnerApp.API/Migrations/20210530140244_läggForeignKeyPhoto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HittaPartnerApp.API.Migrations
+{
+    public partial class läggForeignKeyPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

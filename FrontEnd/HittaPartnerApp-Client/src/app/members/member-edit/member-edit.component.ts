19268530f4c0bca14997d9f574dd45b8e2f1c459
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-03 15:35:52 +0200

    updatera användaren info edit-component

--- FrontEnd/HittaPartnerApp-Client/src/app/members/member-edit/member-edit.component.ts
+++ FrontEnd/HittaPartnerApp-Client/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,12 +11,18 @@ import { User } from 'src/app/_models/user';
 })
 export class MemberEditComponent implements OnInit {
 user!:User;
-  constructor(private route:ActivatedRoute) { }
+@ViewChild ('editForm') editForm:NgForm | undefined
+  constructor(private route:ActivatedRoute,private alertify:AlertifyService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data=>{
       this.user= data['user'];
     })
+  }
+  updateUser()
+  {
+    this.editForm?.reset(this.user)
+    this.alertify.success("Profilen har ändrats")
   }
 
 }

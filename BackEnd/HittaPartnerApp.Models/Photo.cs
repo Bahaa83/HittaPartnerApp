ca8caed33545cdd220dacf6df2b1af89522c3895
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-04 15:34:52 +0200

    lägg til ett nytt kunskap i photo klass

--- BackEnd/HittaPartnerApp.Models/Photo.cs
+++ BackEnd/HittaPartnerApp.Models/Photo.cs
@@ -5,6 +5,7 @@ namespace HittaPartnerApp.Models
     public class Photo:Record
     {
         public string Url { get; set; }
+        public string PublicId { get; set; }
         public string Description { get; set; }
         public DateTime  DateAdded { get; set; }
         public bool IsMain { get; set; }

bc8e6f71bb5eedfa8e45b3cf937c12aa2fcd5ac7
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-04 18:32:42 +0200

    Utföra ladda up foto för användaren

--- BackEnd/HittaPartnerApp.API/Controllers/PhotosController.cs
+++ BackEnd/HittaPartnerApp.API/Controllers/PhotosController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using HittaPartnerApp.API.Data;
 using HittaPartnerApp.API.Helpers;
+using HittaPartnerApp.API.Services.DtoModels;
 using HittaPartnerApp.API.Services.IRepositories;
+using HittaPartnerApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace HittaPartnerApp.API.Controllers
@@ -22,6 +26,7 @@ namespace HittaPartnerApp.API.Controllers
         private readonly IHittaPartnerRepo _hittaPartnerRepo;
         private readonly IOptions<CloudinarySettings> _cloudinarySettings;
         private readonly IMapper _mapper;
+        private Cloudinary cloudinary;
 
         public PhotosController(IHittaPartnerRepo  hittaPartnerRepo,IOptions< CloudinarySettings> cloudinarySettings,IMapper mapper)
         {
@@ -34,9 +39,45 @@ namespace HittaPartnerApp.API.Controllers
                 _cloudinarySettings.Value.ApiSecret
                 );
 
-            Cloudinary cloudinary = new Cloudinary(account);
+             cloudinary = new Cloudinary(account);
         }
 
+        [HttpPost("AddPhotoForUser")]
+        [Obsolete]
+        public async Task<ActionResult> AddPhotoForUser(string userId,PhotoForUserDto photoForUserDto)
+        {
+            if (userId != User.FindFirst(ClaimTypes.NameIdentifier).Value)
+            {
+                return Unauthorized();
+            }
+           var  userFromRepo = await _hittaPartnerRepo.GetUserByID(userId);
+            var file = photoForUserDto.File;
+            var uploadResult = new ImageUploadResult();
+            if(file!=null&& file.Length>0)
+            {
+                 using(var stream = file.OpenReadStream())
+                {
+                    var uploadparams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.FileName, stream),
+                        Transformation = new Transformation()
+                        .Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+                    uploadResult = cloudinary.Upload(uploadparams);
+                }
+            }
+            photoForUserDto.Url = uploadResult.Uri.ToString();
+            photoForUserDto.PublicId = uploadResult.PublicId;
+            var photo = _mapper.Map<Photo>(photoForUserDto);
+            if (!userFromRepo.Photos.Any(p => p.IsMain))
+                photo.IsMain = true;
+            userFromRepo.Photos.Add(photo);
+            if (await _hittaPartnerRepo.SaveAll())
+                return Ok();
+            return BadRequest("Fel vid tillägg av bild");
+
+        }
         
+
     }
 }

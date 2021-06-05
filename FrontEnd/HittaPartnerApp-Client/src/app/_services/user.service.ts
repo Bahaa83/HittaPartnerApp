5ab9556e56711c18313f4eb71a994e0b0969eb72
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-05 19:11:51 +0200

    setMainPhoto funktion

--- FrontEnd/HittaPartnerApp-Client/src/app/_services/user.service.ts
+++ FrontEnd/HittaPartnerApp-Client/src/app/_services/user.service.ts
@@ -8,18 +8,24 @@ import { User } from '../_models/user';
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-baseUrl=environment.apiUrl+'Users/';
-constructor(private http:HttpClient) { }
-getAllUsers():Observable<User[]>
-{
-return this.http.get<User[]>(this.baseUrl+'GetAllUsers');
-}
-getUserById(id:string):Observable<User>
-{
-return this.http.get<User>(this.baseUrl+'GetUserByID?userId='+id);
-}
-updateUser(id:string,user:User){
-  return this.http.put(this.baseUrl+'UpdateUser?userID=' +id,user)
-}
-}
+      export class UserService {
+      baseUrl=environment.apiUrl+'Users/';
+      photobaseUrl=environment.apiUrl;
+      constructor(private http:HttpClient) { }
+      getAllUsers():Observable<User[]>
+      {
+      return this.http.get<User[]>(this.baseUrl+'GetAllUsers');
+      }
+        getUserById(id:string):Observable<User>
+        {
+        return this.http.get<User>(this.baseUrl+'GetUserByID?userId='+id);
+        }
+          updateUser(id:string,user:User){
+            return this.http.put(this.baseUrl+'UpdateUser?userID=' +id,user)
+          }
+            setMainPhoto(userId:string,photoId:string){
+              return this.http.post(this.photobaseUrl+'Photos/SetMainPhotoForUser?userId='+userId+'&photoId=',photoId);
+            }
+
+
+ }

10f212bb94d13df75e540a9eadd6bc1a6315c7a0
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-08 00:50:48 +0200

    Tilääg nya kunskaper i UserForRegisterDto

--- BackEnd/HittaPartnerApp.API/Services/DtoModels/UserForRegisterDto.cs
+++ BackEnd/HittaPartnerApp.API/Services/DtoModels/UserForRegisterDto.cs
@@ -13,5 +13,25 @@ namespace HittaPartnerApp.API.Services.DtoModels
         [StringLength(10,MinimumLength =4,ErrorMessage ="Låsenördet får inte bestå av mer än tiotecken och inte mindre än fyra")]
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+
+        }
+
     }
 }

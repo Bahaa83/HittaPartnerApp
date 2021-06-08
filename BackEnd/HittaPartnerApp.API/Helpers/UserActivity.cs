ccb362a688b94d11d35caf4a4862e8c829d23c3d
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-08 22:05:58 +0200

    Skapa UserActivity klass och implementera den av IAsyncActionFilter

---
+++ BackEnd/HittaPartnerApp.API/Helpers/UserActivity.cs
@@ -0,0 +1,24 @@
+﻿using HittaPartnerApp.API.Services.IRepositories;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HittaPartnerApp.API.Helpers
+{
+    public class UserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+            var userId = resultContext.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var repo = resultContext.HttpContext.RequestServices.GetService<IHittaPartnerRepo>();
+            var currentUser = await repo.GetUserByID(userId);
+            currentUser.LastActive = DateTime.Now;
+            await repo.SaveAll();
+        }
+    }
+}

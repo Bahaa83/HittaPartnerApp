b2a2f6d58c0ab1bf311b771323eabff80dd6a1c8
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-12 02:02:52 +0200

    Skapa Like Klass

---
+++ BackEnd/HittaPartnerApp.Models/Like.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HittaPartnerApp.Models
+{
+   public class Like
+    {
+        public string LikerID { get; set; }
+        public string LikeeID { get; set; }
+        public User Liker { get; set; }
+        public User Likee { get; set; }
+    }
+}

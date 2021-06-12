ee9f3fb1502248775f84558d7ba45271613a7ae5
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-12 19:08:02 +0200

    Implementera send like

--- FrontEnd/HittaPartnerApp-Client/src/app/members/member-card/member-card.component.ts
+++ FrontEnd/HittaPartnerApp-Client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-member-card',
@@ -10,8 +13,15 @@ import { User } from 'src/app/_models/user';
 export class MemberCardComponent implements OnInit {
   @Input()
   user!: User; 
-  constructor(private rout:Router) { }
+  constructor(private uthService:AccountService, private userService :UserService,private alertify:AlertifyService) { }
 
   ngOnInit() {
+
+  }
+  sendLike(id:string){
+    this.userService.sendLike(this.uthService.decodedToken.nameid,id).subscribe(
+      data=>{this.alertify.success('Du gillade '+this.user.knownAs)},
+      error=>{this.alertify.error(error)}
+    )
   }
 }

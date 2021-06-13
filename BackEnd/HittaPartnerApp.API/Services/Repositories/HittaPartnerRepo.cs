99d118619a3bc36f1cd61c3bb351e56fbed70086
bahaa.abokhaled83@gmail.com <bahaa.abokhaled83@gmail.com>
2021-06-14 00:51:54 +0200

    Små fix i kode

--- BackEnd/HittaPartnerApp.API/Services/Repositories/HittaPartnerRepo.cs
+++ BackEnd/HittaPartnerApp.API/Services/Repositories/HittaPartnerRepo.cs
@@ -132,14 +132,14 @@ namespace HittaPartnerApp.API.Services.Repositories
             switch (messageParams.MessageType)
             {
                 case "Inbox":
-                    messages = messages.Where(m => m.RecipientID==messageParams.UserId);
+                    messages = messages.Where(m => m.RecipientID.Equals(messageParams.UserId));
                     break;
                 case "Outbox":
-                    messages = messages.Where(m => m.SenderID==messageParams.UserId);
+                    messages = messages.Where(m => m.SenderID.Equals(messageParams.UserId));
                     break;
                
                 default :
-                    messages = messages.Where(m => m.RecipientID==messageParams.UserId && m.IsRead==false);
+                    messages = messages.Where(m => m.RecipientID.Equals(messageParams.UserId) && m.IsRead==false);
                     break;
             }
             messages = messages.OrderByDescending(m => m.MessageSent);
